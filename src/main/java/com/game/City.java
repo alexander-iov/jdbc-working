373cb49c29a1b5e2a825dc925405f80caabdbfe3
alexander-iov <sasha.iovsa@gmail.com>
2021-02-12 18:57:09 +0200

    Add City

---
+++ src/main/java/com/game/City.java
@@ -0,0 +1,20 @@
+package com.game;
+
+public final class City {
+
+    private final String id;
+    private final String name;
+
+    public City(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
