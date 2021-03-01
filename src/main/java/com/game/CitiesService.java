d7a4ecdff637e236f9ff9985f4f53256c5e5dc19
alexander-iov <sasha.iovsa@gmail.com>
2021-03-01 16:48:24 +0200

    -throws

--- src/main/java/com/game/CitiesService.java
+++ src/main/java/com/game/CitiesService.java
@@ -18,7 +18,7 @@ public class CitiesService {
         }
     }
 
-    public void addCity(City city, Connection connection) throws SQLException {
+    public void addCity(City city, Connection connection) {
         String query = "INSERT INTO cities VALUES ('" + city.getId() + "', '" + city.getName() + "');";
         try (Statement statement = connection.createStatement()) {
             statement.execute(query);
